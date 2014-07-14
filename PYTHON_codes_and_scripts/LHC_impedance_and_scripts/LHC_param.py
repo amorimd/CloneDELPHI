d00041c359a37a921b4576940d217ed7543157f8
nmounet <nicolas.mounet@cern.ch>
2014-07-14 15:04:22 +0200

    LHC impedance and scripts: documentation added

--- PYTHON_codes_and_scripts/LHC_impedance_and_scripts/LHC_param.py
+++ PYTHON_codes_and_scripts/LHC_impedance_and_scripts/LHC_param.py
@@ -14,6 +14,30 @@ from DELPHI import Qs_from_RF_param
 
 def LHC_param(E0,E=7e12):
 
+    ''' generate typical LHC parameters, given the proton rest energy E0 in J (from e.g. function
+    proton_param) and the beam energy E in eV.
+    Outputs:
+    - machine: string with machine name,
+    - E: same as input (beam energy in eV),
+    - gamma: relativistic mass factor,
+    - sigmaz: RMS bunch length in m,
+    - taub: total bunch length in s (4*RMS),
+    - R: machine pysical radius (circumference/(2 pi)),
+    - Qx: total horizontal tune (integer + fractional parts),
+    - Qxfrac: fractional horizontal tune,
+    - Qy: total vertical tune (integer + fractional parts),
+    - Qyfrac: fractional vertical,
+    - Qs: synchrotron tune,
+    - eta: slippage factor (alpha_p-1/gamma^2),
+    - f0: revolution frequency,
+    - omega0: revolution angular frequency=2pi*f0,
+    - omegas: synchrotron angular frequency=Qs*omega0,
+    - dphase: phase of damper w.r.t. "normal" purely resistive damper,
+    - Estr: string with energy (e.g. '7TeV', '450GeV', etc. - see below),
+    - V: RF voltage in V,,
+    - h: harmonic number.
+    '''
+    
     # E is the energy in eV, V the RF voltage in V
     if (E==7e12): V=16e6;Estr='7TeV';taub=1.e-9; # full length in s
     elif (E==6.5e12): V=16e6;Estr='6p5TeV';taub=1.e-9; # full length in s
